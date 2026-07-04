c10f5f931a83b701b11ed834b0057f3f4613a3fd
onsen2026 <sumica.jp@outlook.com>
2026-07-04 11:08:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-document.querySelector("button").addEventListener("click", () => {
-    alert("検索機能は現在準備中です。");
+const input = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".card");
+
+input.addEventListener("input", function () {
+    const keyword = this.value.toLowerCase();
+
+    cards.forEach(card => {
+        const text = card.getAttribute("data-keyword").toLowerCase();
+
+        if (text.includes(keyword)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
 });
