9d3924511e9994cbf34b1ea9cf672635936f9d57
onsen2026 <sumica.jp@outlook.com>
2026-07-04 13:59:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,26 +15,48 @@ searchButton.addEventListener("click", () => {
   alert(`${keyword} を検索します`);
 });
 
-prefButtons.forEach((button) => {
+const prefLinks = {
+  "北海道":"hokkaido.html",
+  "青森":"aomori.html",
+  "岩手":"iwate.html",
+  "宮城":"miyagi.html",
+  "秋田":"akita.html",
+  "山形":"yamagata.html",
+  "福島":"fukushima.html",
+
+  "東京":"tokyo.html",
+  "神奈川":"kanagawa.html",
+  "埼玉":"saitama.html",
+  "千葉":"chiba.html",
+
+  "京都":"kyoto.html",
+  "大阪":"osaka.html",
+  "兵庫":"hyogo.html",
+
+  "広島":"hiroshima.html",
+  "山口":"yamaguchi.html",
+
+  "福岡":"fukuoka.html",
+  "佐賀":"saga.html",
+  "長崎":"nagasaki.html",
+  "熊本":"kumamoto.html",
+  "大分":"oita.html",
+  "宮崎":"miyazaki.html",
+  "鹿児島":"kagoshima.html",
+  "沖縄":"okinawa.html"
+};
+
+prefButtons.forEach(button => {
 
   button.addEventListener("click", () => {
 
     const pref = button.textContent;
 
-    if (pref === "福岡") {
-      location.href = "fukuoka.html";
+    if(prefLinks[pref]){
+      location.href = prefLinks[pref];
     }
-
-    else if (pref === "大分") {
-      location.href = "oita.html";
-    }
-
-    else if (pref === "広島") {
-      location.href = "hiroshima.html";
-    }
-
-    else {
-      alert(`${pref} の温泉情報は準備中です`);
+    else{
+      alert("ページ準備中です");
     }
 
   });
